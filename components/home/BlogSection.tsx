dfd8e966c922dffcb7f86f19ad7d017ae8e62567
SangTran-127 <tranquangsang12.7@gmail.com>
2022-12-29 05:34:37 +0700

    up

--- components/home/BlogSection.tsx
+++ components/home/BlogSection.tsx
@@ -7,12 +7,21 @@ interface BlogSectionProps {}
 
 export default async function BlogSection (props: BlogSectionProps) {
     const blogList = await getBlogList();
-    const lastestBlog = blogList.reduce((prevBlog, blog) => Number(prevBlog.publishedDate) > Number(blog.publishedDate) ? prevBlog : blog)
-    console.log(lastestBlog.publishedDate);
+    const sectionList = blogList.sort((prevBlog, currentBlog) => Number(currentBlog.publishedDate) - Number(prevBlog.publishedDate))
+
+    sectionList.forEach((b) => {
+        console.log(b.publishedDate);
+        
+    })
+    
     return (
         <Container tailWindClass='max-w-screen-lg md:px-20 mx-auto hero-container md:flex md:justify-between md:items-center px-3'>
             <>
-                <h1>Caksdk</h1>
+                <h1>Stories & Blog</h1>
+                <p>The lastest blog</p>
+                <div className='grid'>
+
+                </div>
             </>
         </Container>
     )
