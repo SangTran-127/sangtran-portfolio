f4546c19626ddaac16566b00267aaae4c55aaa37
SangTran-127 <tranquangsang12.7@gmail.com>
2022-12-29 06:04:04 +0700

    up

---
+++ components/blog/BlogCard.tsx
@@ -0,0 +1,15 @@
+import { Post } from '@/models'
+import React from 'react'
+
+interface BlogCardProps {
+    blog: Post
+    lastest: boolean
+}
+
+export default function BlogCard ({blog}: BlogCardProps) {
+
+  return (
+    <div>{blog.title}</div>
+  )
+}
+
--- components/home/BlogSection.tsx
+++ components/home/BlogSection.tsx
@@ -1,26 +1,42 @@
 import React from 'react'
 import { getBlogList } from '@/utils';
 import Container from '../layout/Container';
+import BlogCard from '../blog/BlogCard';
+
+
+
 interface BlogSectionProps {}
 
 
-
 export default async function BlogSection (props: BlogSectionProps) {
+
     const blogList = await getBlogList();
-    const sectionList = blogList.sort((prevBlog, currentBlog) => Number(currentBlog.publishedDate) - Number(prevBlog.publishedDate))
+    // get lastest day
+    const sectionList = blogList.sort((prevBlog, currentBlog) => new Date(prevBlog.publishedDate).getTime() - new Date(currentBlog.publishedDate).getTime())
 
     sectionList.forEach((b) => {
         console.log(b.publishedDate);
         
     })
-    
+    const [lastestBlog, ...restBlog] = sectionList
     return (
-        <Container tailWindClass='max-w-screen-lg md:px-20 mx-auto hero-container md:flex md:justify-between md:items-center px-3'>
+        <Container tailWindClass='max-w-screen-lg md:px-20 mx-auto px-3'>
             <>
-                <h1>Stories & Blog</h1>
-                <p>The lastest blog</p>
-                <div className='grid'>
+                <h1 className='text-dracula-light text-center text-3xl'>Stories & Blog</h1>
+                <p className='text-dracula-dark-300 text-center'>The lastest blog</p>
+                <div className='grid grid-cols-2 grid-rows-3'>
+                    <div>
+                        <BlogCard blog={lastestBlog}/>
+                    </div>
 
+                    <div>
+                        {restBlog.map((blog) => (
+                            <div key={blog.title}>
+                                <BlogCard blog={blog} />
+                            </div>
+                        ))}
+                    </div>
+                  
                 </div>
             </>
         </Container>
