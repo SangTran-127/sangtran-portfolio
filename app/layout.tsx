aff6146342d2ddeb24087d0f4999d980d7c4c69f
SangTran-127 <tranquangsang12.7@gmail.com>
2023-01-01 14:20:34 +0700

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/header";
 import { Be_Vietnam_Pro } from "@next/font/google";
-import "./global.css";
-import "./prism.css";
+import "@/styles/global.css";
+import "@/styles/prism.css";
 import "@/sass/index.scss";
 import "animate.css";
 const vietnamese = Be_Vietnam_Pro({
